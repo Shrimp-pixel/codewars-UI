88c86960ae68f88d189b4cc270190b8a93314aec
Shrimp-pixel <aleksander.17047665@gmail.com>
2022-12-21 18:26:48 +0300

    init

--- tests/test_data/users.py
+++ tests/test_data/users.py
@@ -1,42 +1,15 @@
+import os
 from dataclasses import dataclass
-from enum import Enum
-from typing import Tuple
+from dotenv import load_dotenv
 
-
-class Subject(Enum):
-    History = 'History'
-    Maths = 'Maths'
-    Physics = 'Pysics'
-
-
-class Hobby(Enum):
-    Sports = '1'
-    Reading = '2'
-    Music = '3'
-
-
-class Gender(Enum):
-    Male = 'Male'
-    Female = 'Female'
-    Other = 'Other'
+load_dotenv()
 
 
 @dataclass
 class User:
-    gender: Gender
-    name: str
-    last_name: str = 'YouMeanIt'
-    email: str = 'abc@efg.com'
-    user_number: str = '0123456789'
-    birth_day: str = '30'
-    birth_month: str = 'September'
-    birth_year: str = '1972'
-    subjects: Tuple[Subject] = (Subject.History,)
-    current_address: str = 'bla bla bla'
-    hobbies: Tuple[Hobby] = (Hobby.Sports,)
-    picture_file: str = 'photo.jpg'
-    state: str = 'Haryana'
-    city: str = 'Karnal'
+    email: str = os.getenv('CODEWARS_EMAIL')
+    username: str = os.getenv('CODEWARS_LOGIN')
+    password: str = os.getenv('CODEWARS_PASSWORD')
 
 
-yuri = User(name='yuri', gender=Gender.Male)
+newUser = User()
